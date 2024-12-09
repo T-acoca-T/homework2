717420b10338bc4bef73fbd2354e6d1ed44c1894
T-acoca-T <bosofia92@gmail.com>
2024-12-09 19:08:31 +0100

    Add files via upload

---
+++ less_2hw.py
@@ -0,0 +1,127 @@
+import random
+
+class Cat:
+    def __init__(self, name, age, hunger, strength, health):
+        self.name = name
+        self.age = age
+        self.hunger = hunger
+        self.strength = strength
+        self.health = health
+
+
+
+class Dog:
+    def __init__(self, name, age, hunger, strength, health):
+        self.name = name
+        self.age = age
+        self.hunger = hunger
+        self.strength = strength
+        self.health = health
+
+
+
+Schlimbo = Cat("Schlimbo", '1', '100', '43', '100')
+
+print(Schlimbo.name)
+print("Sclimbo is a cat")
+print("I think")
+print("I hope")
+print("anyway...")
+print("Age:")
+print(Schlimbo.age)
+print("Hunger:")
+print(Schlimbo.hunger)
+print("Strength:")
+print(Schlimbo.strength)
+print("He's not very strong")
+print("But It's ok")
+print("Because Schlimbo got D_E_T_E_R_M_I_N_A_T_I_O_N")
+print(".")
+print(".")
+print(".")
+print("Unlike others")
+
+
+
+Kyle = Dog("Kyle", '11', '54', '31', '83')
+RAT = Dog("RAT", '6', '47', '65', '97')
+
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print("                   they're watching...")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+
+print("Schlimbo has done nothing I_M_P_O_R_T_A_N_T enough for a couple of years")
+Schlimbo = Cat("Schlimbo", '3', '82', '43', '100')
+
+print("Age:")
+print(Schlimbo.age)
+print("Hunger:")
+print(Schlimbo.hunger)
+print("Strength:")
+print(Schlimbo.strength)
+print("He hasn't eaten yet")
+print("He's a bit hungry")
+
+food_list = {
+    "bird": {"hunger": +10},
+    "mouse": {"hunger": +6},
+    "cat-food": {"hunger": +4},
+    "water": {"hunger": +3}
+}
+
+#print(food_list)
+#class Foodchoice:
+ #   def __init__(self, food_list):
+  #      self.food_list = random.choice(food_list)
+
+#Foodchoice = Foodchoice(food_list)
+#print(Foodchoice.food_list)
+
+print("Schlimbo chose bird")
+Schlimbo = Cat("Schlimbo", '3', '92', '43', "100")
+print("Hunger:")
+print(Schlimbo.hunger)
+print(".")
+print(".")
+print(".")
+print("Oh no!")
+print("Schlimbo encountered the stray dog RAT")
+print("Schlimbo's health:")
+print(Schlimbo.health)
+print("RAT's health:")
+print(RAT.health)
+print("RAT attacks")
+print("Schlimbo's health:")
+Schlimbo = Cat("Schlimbo", '3', '92', '43', "41")
+print(Schlimbo.health)
+
+print("Shlimbo fights back!")
+RAT = Dog("RAT", '6', '47', '65', '57')
+print("RAT's health:")
+print(RAT.health)
+print("RAT doesn't give up and deals a final blow to the neck")
+print("Schlimbo's health:")
+Schlimbo = Cat("Schlimbo", '3', '92', '19', "1")
+print(Schlimbo.health)
+print("RAT leaves Schlimbo to bleed out")
+print(".")
+print(".")
+print(".")
+print("Schlimbo's health:")
+Schlimbo = Cat("Schlimbo", '3', '54', '1', "0")
+print("Schlimbo's health:")
+print(Schlimbo.health)
+print("Schlimbo's strength:")
+print(Schlimbo.strength)
+print("                               SCHLIMBO DIED")
+print("                           why didn't you help him?")
+
+
